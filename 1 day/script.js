402e503ac02c99167ca8a2a17ae902ef5fda1879
gohel-darshan <darshanrgohel@gmail.com>
2025-03-27 18:52:09 +0530

    first commit

---
+++ 1 day/script.js
@@ -0,0 +1,8 @@
+// console.log("hello world")
+// alert("purvajit is always available on the server side and  will be available on the client side")
+// document.write("hello world")
+
+let box = document.getElementById("a")
+box.style="color: red"
+box.style.borderRadius(50)
+box.textContent = "Hello world"
